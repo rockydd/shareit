9b7924545931d6d0daae7b61d5d36f3366ffd3c1
rockydd <rockydd@gmail.com>
2009-05-28 09:58:44 +0800

    added link for items in index page. added a tag picture

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,9 @@
 module ItemsHelper
+  def item_picture(item)
+    if(item.attachment_content_type =~ /^image/)
+      return item.attachment.url
+    else
+      return item.thumb_url
+    end
+  end
 end
